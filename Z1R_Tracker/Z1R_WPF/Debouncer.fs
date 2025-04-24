10a290ad1ea74671d975a66e6b73a40f4edb309c
Neononane <ryan@jimmlr.com>
2025-04-24 13:24:28 -0400

    Added a debouncer for future use

---
+++ Z1R_Tracker/Z1R_WPF/Debouncer.fs
@@ -0,0 +1,15 @@
+﻿module Debouncer
+
+type Debouncer(delayMs: int) =
+    let mutable currentCts : System.Threading.CancellationTokenSource option = None
+
+    member this.Trigger(action: unit -> unit) =
+        currentCts |> Option.iter (fun cts -> cts.Cancel())
+        let newCts = new System.Threading.CancellationTokenSource()
+        currentCts <- Some newCts
+
+        async {
+            do! Async.Sleep delayMs
+            if not newCts.IsCancellationRequested then
+                action()
+        } |> Async.Start
